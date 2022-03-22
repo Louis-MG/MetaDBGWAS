ca6d93b79875a04125d8a4e4a569edbc87f9efbc
Louis-MG <lm.gueguen@orange.fr>
2022-03-22 14:36:47 +0100

    modified text color for install

--- install.sh
+++ install.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-GREEN='\132[1;32m'
+GREEN='\132[1;32m' # green color
+NC='\033[0m' # No Color
 
 #lighter compilation
-echo '${GREEN}Starting Lighter installation'
+echo "${GREEN}Starting Lighter installation${NC}"
 cd Lighter
 make
 
 cd ..
 
-echo '${GREEN}Starting bcalm2 installation'
+echo "${GREEN}Starting bcalm2 installation${NC}"
 #bcalm compilation
 cd bcalm
 mkdir build;  cd build;  cmake ..;  make -j 4
 
 cd ../..
 
-echo '${GREEN}Starting REINDEER installation'
+echo "${GREEN}Starting REINDEER installation${NC}"
 cd REINDEER
 make
 
 cd ..
 
-echo '${GREEN}Starting DBGWAS installation'
+echo "${GREEN}Starting DBGWAS installation${NC}"
 mkdir DBGWAS
 cd DBGWAS
 curl  https://www.dropbox.com/s/s9oojqfl1kgi4l5/DBGWAS-0.5.4-Linux-precompiled.tar.gz?dl=1 -L --output ./dbgwas.Linux.precompiled.tar.gz
@@ -30,10 +31,10 @@ tar -zxvf dbgwas.Linux.precompiled.tar.gz
 rm dbgwas.Linux.precompiled.tar.gz
 cd ..
 
-echo '${GREEN}Compiling MetaDBGWAS in src/'
+echo "${GREEN}Compiling MetaDBGWAS in src/${NC}"
 cmake .
 make -j 4
 
 cd ..
 
-echo "${GREEN}Installation complete ! Do not forget to install the R and C++ libraries."
+echo "${GREEN}Installation complete ! Do not forget to install the R and C++ libraries.${NC}"
