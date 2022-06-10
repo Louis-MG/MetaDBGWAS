85668db1f6a840d9320b95af6a9f88fdceeb2337
Louis-MG <lm.gueguen@orange.fr>
2022-06-10 13:42:40 +0200

    update dockerfile and install

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -2,8 +2,8 @@ apt-get update
 apt install -y libgatbcore-dev libhdf5-dev libboost-all-dev libpstreams-dev zlib1g-dev g++ cmake git r-base-core
 Rscript -e "install.packages(c('ape', 'phangorn'))"
 Rscript -e "install.packages('https://raw.githubusercontent.com/sgearle/bugwas/master/build/bugwas_1.0.tar.gz', repos=NULL, type='source')"
-git clone --recursive https://github.com/Louis-MG/MetaDBGWAS.git
-cd MetaDBGWAS
+git clone --recursive https://github.com/Louis-MG/Metadbgwas.git
+cd Metadbgwas
 chmod +x ./tools/phantomjs/phantomjs
 chmod +x ./tools/gemma/gemma.0.93b
 sed -i "51i#include <limits>" ./REINDEER/blight/robin_hood.h #temporary fix for REINDEER compilation
