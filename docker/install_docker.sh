c60d0492efbb652e9f127e959eae81be8f99782d
Louis-MG <lm.gueguen@orange.fr>
2022-06-10 14:30:39 +0200

    test

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -4,6 +4,7 @@ Rscript -e "install.packages(c('ape', 'phangorn'))"
 Rscript -e "install.packages('https://raw.githubusercontent.com/sgearle/bugwas/master/build/bugwas_1.0.tar.gz', repos=NULL, type='source')"
 git clone --recursive https://github.com/Louis-MG/Metadbgwas.git
 cd Metadbgwas
+chmod +x metadbgwas.sh
 chmod +x ./tools/phantomjs/phantomjs
 chmod +x ./tools/gemma/gemma.0.93b
 sed -i "51i#include <limits>" ./REINDEER/blight/robin_hood.h #temporary fix for REINDEER compilation
