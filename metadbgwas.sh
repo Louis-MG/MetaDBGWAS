b9f42717316d2e52dbed959a7556cc419cac9eb8
lmgueguen <lm.gueguen@orange.fr>
2023-06-13 15:53:32 -0400

    fixed counter mistake in bcalm2 part

--- metadbgwas.sh
+++ metadbgwas.sh
@@ -294,8 +294,9 @@ then
 		echo "${counter}/${counter_total} Processing files ..."
 	        $metadbgwas_path/bcalm/build/bcalm -in $i -kmer-size $kmer -nb-cores $threads  $verbosity_level -abundance-min 1 # TODO: add the -out option to give prefix and avoid moving files around
 		mv *.unitigs.fa $output/unitigs
-		tput cuu 7
+		tput cuu 6
 		tput ed
+		((counter++))
 	done
 	find $output/unitigs/*.unitigs.fa -type f > $output/unitigs/fof_unitigs_index.txt
 	#the option abundance min is used to keep all kmers: we already corrected them, and not keeping them all to build the unitigs would have 2 unfortunate consequences :
